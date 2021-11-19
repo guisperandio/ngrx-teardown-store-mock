89f92287dbdf1650f67b844a50ef0db6fcc5268a
Guilherme Santos <guilherme.santos@sap.com>
2021-11-19 11:39:34 +0000

    initial commit

---
+++ apps/sample-store-mock/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngrx-teardown-store-mock-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'sample-store-mock';
+}
